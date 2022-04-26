5d3be41014ee691a263d962ba8cbb0a43a25229b
brunocesarangst <bruno.angst@rede.ulbra.br>
2022-04-26 00:50:23 -0300

    login 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <>
       <Head>
@@ -32,7 +36,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   )
 }
