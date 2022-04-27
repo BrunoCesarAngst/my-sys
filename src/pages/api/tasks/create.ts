012787c3262517e9247d1e15e40ec7a0783e0c51
brunocesarangst <bruno.angst@rede.ulbra.br>
2022-04-27 08:37:13 -0300

    deploy1

--- src/pages/api/tasks/create.ts
+++ src/pages/api/tasks/create.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../lib/prisma";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
